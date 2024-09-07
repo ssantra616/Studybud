f92bfcc2e383edbbe3eb170a2f7dbda2c8dcbc45
ssantra616 <ssantra@charlotte.edu>
2024-09-06 23:37:18 -0400

    Completed Views & URLs

--- studybud/urls.py
+++ studybud/urls.py
@@ -1,13 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
-from django.conf.urls.static import static
+from django.urls import path
+
+from base.views import home, room
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls')),
-    path('api/', include('base.api.urls'))
+    path('', home),
+    path('room/', room)   
 ]
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
