6e101f3455dd89b2c4ab97825adb3d6dfddba877
ssantra616 <ssantra@charlotte.edu>
2024-09-17 00:47:50 -0400

    Chat Room Messages CRUD

--- base/models.py
+++ base/models.py
@@ -16,8 +16,8 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    #participants = 
-    update = models.DateTimeField(auto_now=True)
+    participants = models.ManyToManyField(User, related_name='participants', blank=True) 
+    updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
@@ -30,8 +30,11 @@ class Message(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     room = models.ForeignKey(Room, on_delete=models.CASCADE)
     body = models.TextField()
-    update = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-updated', '-created']
 
     def __str__(self):
         return self.body[0:50]
