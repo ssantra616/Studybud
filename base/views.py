475e4576fc69b27d7f6f83b1e37696a378cbc16b
ssantra616 <ssantra@charlotte.edu>
2024-09-17 02:07:35 -0400

    User Profile Page

--- base/views.py
+++ base/views.py
@@ -77,7 +77,7 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
-    room_messages = Message.objects.all()
+    room_messages = Message.objects.filter(Q(room_topic_name_icontains=q))
 
     context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages':room_messages}
     return render(request, 'base/home.html', context)
@@ -99,6 +99,10 @@ def room(request, pk):
 
     context = {'room': room, 'room_messages':room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
+
+def userProfile(request):
+    context = {}
+    return render(request, 'base/profile.html', context)
 
 @login_required(login_url='login')
 def createRoom(request):
