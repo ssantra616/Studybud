6a76140b9e6f88db600e9fe6266de5e7def46dee
ssantra616 <ssantra@charlotte.edu>
2024-09-17 01:32:56 -0400

    Activity Feed

--- base/views.py
+++ base/views.py
@@ -77,21 +77,27 @@ def home(request):
 
     topics = Topic.objects.all()
     room_count = rooms.count()
+    room_messages = Message.objects.all()
 
-    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count}
+    context = {'rooms': rooms, 'topics': topics, 'room_count': room_count, 'room_messages':room_messages}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
     room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
 
     if request.method == 'POST':
         message = Message.objects.create(
-
+            user=request.user,
+            room=room,
+            body=request.POST.get('body')
         )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
 
-    context = {'room': room, 'messages':room_messages}
+    context = {'room': room, 'room_messages':room_messages, 'participants':participants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='login')
@@ -137,3 +143,17 @@ def deleteRoom(request,pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+
+@login_required(login_url='login')
+def deleteMessage(request,pk):
+    message = Message.objects.get(id=pk)
+
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here :( )')
+
+
+    if request.method == "POST":
+        message.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': message})
